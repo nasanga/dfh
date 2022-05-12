53828e0d08be824c1f3bad2ac141b984b0b8dd51
nasanga <105359518+nasanga@users.noreply.github.com>
2022-05-12 08:10:42 +0000

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -67,8 +67,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-        buttons.buildbutton("Asana Udara", "#")
-    buttons.buildbutton("Dev", "https://t.me/+PRRzqHd31XY3ZWZk")
+        buttons.buildbutton("Asana Udara", "https://asangabro.ga")
+    buttons.buildbutton("Dev", "https://asangaudara.ga")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
